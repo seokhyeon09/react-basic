4b79090a3b00e83696bde7d0753a2ef8a81e7117
seokhyeon09 <ring4392@gmail.com>
2026-02-02 11:35:05 +0900

    React test

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+const Header = () =>{
+    return (
+        <div>Header</div>
+    )
+}
+const Aside = ()=>{
+    return(
+        <aside>
+            asd
+        </aside>
+    )
+}
+export default Header
