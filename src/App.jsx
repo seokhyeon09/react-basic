e081dd9feac65b7f45d991939921a2f72fe84c78
seokhyeon09 <ring4392@gmail.com>
2026-02-12 14:54:07 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import React, {useEffect, useState} from 'react';
 import './App.css'
-import UserList from './Commponent/UserList';
-import PostList from './Commponent/PostList';
-import UserDetail from './Commponent/UserDetail';
-import DummyJson from './Commponent/DummyJson';
+import Count from './Commponent/Count';
+import TodoList from './Commponent/TodoList';
+
 function App() {
   return (
     <div>
-      <h1>axios</h1>
-      {/* <UserList/>
-      <PostList/>
-      <UserDetail/> */}
-      <DummyJson/>
+      <Count/>
+      <TodoList/>
     </div>
   );
 }
